9143376d003a26ae5ffa85f6bad0aff3de4dad30
nunomscs-blip <nunomscs@gmail.com>
2026-07-21 17:12:43 +0100

    0.0.01

--- js/calendario.js
+++ js/calendario.js
@@ -3,7 +3,21 @@ function mostrarCalendario() {
     const calendario = document.getElementById("calendario");
 
     calendario.innerHTML = `
+    
         <h2>Agosto 2026</h2>
+
+        <div id="diasSemana">
+
+            <div>Seg</div>
+            <div>Ter</div>
+            <div>Qua</div>
+            <div>Qui</div>
+            <div>Sex</div>
+            <div>Sáb</div>
+            <div>Dom</div>
+
+        </div>
+
     `;
 
 }
