98bdb2cdf408ef4941670e93df5a1b6ad300cd54
nunomscs-blip <nunomscs@gmail.com>
2026-07-23 16:58:37 +0100

    mes seguinte e anterior

--- js/app.js
+++ js/app.js
@@ -69,6 +69,8 @@ function mesSeguinte(){
 
     mostrarCalendario();
 
+    atualizarBotaoHoje();
+
 }
 
 // =======================================================
@@ -101,24 +103,26 @@ function mesAnterior(){
 
     mostrarCalendario();
 
+    atualizarBotaoHoje();
+
 }
 
-// =======================================================
-// TESTES
-// Utilizado apenas durante o desenvolvimento.
-// =======================================================
+    // =======================================================
+    // TESTES
+    // Utilizado apenas durante o desenvolvimento.
+    // =======================================================
 
-console.log(obterNomeMes(8));
+    console.log(obterNomeMes(8));
 
-const btnCancelarEvento =
+    const btnCancelarEvento =
     document.getElementById("btnCancelarEvento");
 
     btnCancelarEvento.addEventListener(
     "click",
     cancelarEvento
-);
+    );
 
-function cancelarEvento(){
+    function cancelarEvento(){
 
     estado.modo = "visualizacao";
 
@@ -129,6 +133,9 @@ function cancelarEvento(){
         .classList.add("oculto");
 
     mostrarCalendario();
+
+    atualizarBotaoHoje();
+    
 
 }
 
@@ -147,6 +154,61 @@ btnConfiguracao.addEventListener(
 function abrirConfiguracao(){
 
     alert("Configuração");
+
+}
+
+/* ======================================================
+   DADOS DA APLICAÇÃO
+====================================================== */
+
+document.getElementById("tituloApp").textContent = APP.nome;
+
+document.getElementById("rodape").textContent =
+`${APP.nome} • ${APP.ambiente} • ${APP.versao}`;
+
+
+/* ======================================================
+   BOTÃO HOJE
+====================================================== */
+
+const btnHoje = document.getElementById("btnHoje");
+
+btnHoje.addEventListener("click", irParaHoje);
+
+atualizarBotaoHoje();
+
+
+function atualizarBotaoHoje(){
+
+    const hoje = new Date();
+
+    if(
+        calendarioAtual.mes === hoje.getMonth()+1 &&
+        calendarioAtual.ano === hoje.getFullYear()
+    ){
+
+        btnHoje.classList.add("oculto");
+
+    }else{
+
+        btnHoje.classList.remove("oculto");
+
+    }
+
+}
+
+
+function irParaHoje(){
+
+    const hoje = new Date();
+
+    calendarioAtual.mes = hoje.getMonth()+1;
+
+    calendarioAtual.ano = hoje.getFullYear();
+
+    mostrarCalendario();
+
+    atualizarBotaoHoje();
 
 }
 
