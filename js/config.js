140b59068357dbb9b88ef656d9b5e6a564c73101
nunomscs-blip <nunomscs@gmail.com>
2026-07-23 17:03:57 +0100

    js config

---
+++ js/config.js
@@ -0,0 +1,14 @@
+/* ======================================================
+   CONFIGURAÇÃO DA APLICAÇÃO
+====================================================== */
+
+const APP = {
+
+    nome: "BML FireCalendar",
+
+    versao: "v0.2.0",
+
+    ambiente: "ns_production"
+
+};
+
