33ba4b61b68cbf24869b271e1311b073eea7e114
nunomscs-blip <nunomscs@gmail.com>
2026-07-24 11:49:06 +0100

    feat: criar estrutura da camada de persistência local

---
+++ js/database.js
@@ -0,0 +1,39 @@
+
+// =======================================================
+// DATABASE
+// =======================================================
+
+const DATABASE = {
+
+    EVENTOS: "bml_eventos"
+
+};
+
+// =======================================================
+// BASE DE DADOS LOCAL
+// =======================================================
+
+function carregarEventos() {
+
+    // Carrega os eventos do localStorage
+
+}
+
+function guardarEventos() {
+
+    // Guarda os eventos no localStorage
+
+}
+
+function adicionarEvento() {
+
+}
+
+function editarEvento() {
+
+}
+
+function eliminarEvento() {
+
+}
+
